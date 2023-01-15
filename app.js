782fd4c4dcf53f4da8d9f3a01b8561104e6133ea
Amy Lee <39070747+amysujunglee@users.noreply.github.com>
2023-01-14 20:30:29 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const searchBtn = document.querySelector('#search-btn');
 const countryNameInput = document.querySelector('#country-name-input');
+const result = document.querySelector('#result');
 
 searchBtn.addEventListener('click', () => {
     let countryName = 'canada';
@@ -15,5 +16,26 @@ searchBtn.addEventListener('click', () => {
             console.log(data[0].capital[0]);
             console.log(data[0].flags.svg);
             console.log(data[0].name.common);
-    })
+            console.log(data[0].continents[0]);
+            console.log(Object.keys(data[0].currencies)[0]);
+            console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+            console.log(Object.keys(data[0].languages).toString().split(',').join(', '));
+
+            result.innerHTML = `
+                <img src="${data[0].flags.svg}" class="flag-img">
+                <h2>${data[0].name.common}</h2>
+                <div class="wrapper">
+                    <div class="data-wrapper">
+                        <h4>Capital:</h4>
+                        <span>${data[0].capital[0]}</span>
+                    </div>
+                </div>
+                <div class="wrapper">
+                    <div class="data-wrapper">
+                        <h4>Continent:</h4>
+                        <span>${data[0].continents[0]}</span>
+                    </div>
+                </div>
+            `;
+    });
 });
